7246aca070144530759f07d5f44c773d00fe8734
Benton Tripp <bentontripp@gmail.com>
2022-08-22 16:46:02 -0400

    figured out nulls

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import re
 import json
 
@@ -18,14 +19,8 @@ def cleanTable(file: str):
     with open('src/data/durations.json') as f:
         durations = json.loads(f.read())
         f.close()
-    # replace 'nan' with np.nan
-    durations = {
-        k: val for k, val in zip(
-            durations.keys(),
-            list(map(lambda v: v if v != 'nan' else None, durations.values()))
-        )
-    }
-    df["Updated_Duration"] = df.Duration.map(durations)
+    # map to `Updated_Duration`; replace 'nan' with np.nan
+    df["Updated_Duration"] = df.Duration.map(durations).replace({'nan': np.nan})
     return df
 
 
@@ -33,5 +28,5 @@ if __name__ == '__main__':
     date_start = '202107'
     date_end = '202206'
     df = cleanTable(file="src/data/UFOs{}-{}.csv".format(date_start, date_end))
-    print(df.loc[df.Duration == 'Looking at the sky']\
+    print(df.loc[~df.Updated_Duration.isna()]\
         [['Timestamp', 'Posted', 'Duration', 'Updated_Duration']])
